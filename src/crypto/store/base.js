491359ba07bc0ddf74035abde01539fc2b9f837a
Richard van der Hoff <richard@matrix.org>
2017-05-30 23:30:05 +0100

    Storage layer for management of outgoing room key requests

--- src/crypto/store/base.js
+++ src/crypto/store/base.js
@@ -9,3 +9,23 @@
  *
  * @interface CryptoStore
  */
+
+/**
+ * Represents an outgoing room key request
+ *
+ * @typedef {Object} OutgoingRoomKeyRequest
+ *
+ * @property {string} requestId    unique id for this request
+ *
+ * @property {string?} cancellationRequestId
+ *    transaction id for the cancellation, if any
+ *
+ * @property {Array<{userId: string, deviceId: string}>} recipients
+ *    list of recipients for the request
+ *
+ * @property {module:crypto~RoomKeyRequestBody} requestBody
+ *    parameters for the request.
+ *
+ * @property {Number} state   current state of this request (states are defined
+ *    in {@link module:crypto/OutgoingRoomKeyRequestManager~ROOM_KEY_REQUEST_STATES})
+ */
--- src/crypto/store/indexeddb-crypto-store.js
+++ src/crypto/store/indexeddb-crypto-store.js
@@ -15,6 +15,7 @@ limitations under the License.
 */
 
 import q from 'q';
+import utils from '../../utils';
 
 /**
  * Internal module. indexeddb storage for e2e.
@@ -110,6 +111,238 @@ export default class IndexedDBCryptoStore {
             };
         });
     }
+
+    /**
+     * Look for an existing outgoing room key request, and if none is found,
+     * add a new one
+     *
+     * @param {module:crypto/store/base~OutgoingRoomKeyRequest} request
+     *
+     * @returns {Promise} resolves to
+     *    {@link module:crypto/store/base~OutgoingRoomKeyRequest}: either the
+     *    same instance as passed in, or the existing one.
+     */
+    getOrAddOutgoingRoomKeyRequest(request) {
+        const requestBody = request.requestBody;
+
+        return this.connect().then((db) => {
+            const deferred = q.defer();
+            const txn = db.transaction("outgoingRoomKeyRequests", "readwrite");
+            txn.onerror = deferred.reject;
+
+            // first see if we already have an entry for this request.
+            this._getOutgoingRoomKeyRequest(txn, requestBody, {
+                onFound: (existing) => {
+                    // this entry matches the request - return it.
+                    console.log(
+                        `already have key request outstanding for ` +
+                        `${requestBody.room_id} / ${requestBody.session_id}: ` +
+                        `not sending another`,
+                    );
+                    deferred.resolve(existing);
+                },
+                onNotFound: () => {
+                    // we got to the end of the list without finding a match
+                    // - add the new request.
+                    console.log(
+                        `enqueueing key request for ${requestBody.room_id} / ` +
+                        requestBody.session_id,
+                    );
+                    const store = txn.objectStore("outgoingRoomKeyRequests");
+                    store.add(request);
+                    txn.onsuccess = () => { deferred.resolve(request); };
+                },
+            });
+
+            return deferred.promise;
+        });
+    }
+
+    /**
+     * Look for an existing room key request
+     *
+     * @param {module:crypto~RoomKeyRequestBody} requestBody
+     *    existing request to look for
+     *
+     * @return {Promise} resolves to the matching
+     *    {@link module:crypto/store/base~OutgoingRoomKeyRequest}, or null if
+     *    not found
+     */
+    getOutgoingRoomKeyRequest(requestBody) {
+        return this.connect().then((db) => {
+            const deferred = q.defer();
+
+            const txn = db.transaction("outgoingRoomKeyRequests", "readonly");
+            txn.onerror = deferred.reject;
+
+            this._getOutgoingRoomKeyRequest(txn, requestBody, {
+                onFound: (existing) => {
+                    // this entry matches the request - return it.
+                    deferred.resolve(existing);
+                },
+                onNotFound: () => {
+                    deferred.resolve(null);
+                },
+            });
+            return deferred.promise;
+        });
+    }
+
+    _getOutgoingRoomKeyRequest(txn, requestBody, opts) {
+        opts = opts || {};
+        const store = txn.objectStore("outgoingRoomKeyRequests");
+
+        const idx = store.index("session");
+        const cursorReq = idx.openCursor([
+            requestBody.room_id,
+            requestBody.session_id,
+        ]);
+
+        cursorReq.onsuccess = (ev) => {
+            const cursor = ev.target.result;
+            if(!cursor) {
+                if (opts.onNotFound) { opts.onNotFound(); }
+                return;
+            }
+
+            const existing = cursor.value;
+
+            if (utils.deepCompare(existing.requestBody, requestBody)) {
+                if (opts.onFound) { opts.onFound(existing); }
+                return;
+            }
+            cursor.continue();
+        };
+    }
+
+    /**
+     * Look for room key requests by state
+     *
+     * @param {Array<Number>} wantedStates list of acceptable states
+     *
+     * @return {Promise} resolves to the a
+     *    {@link module:crypto/store/base~OutgoingRoomKeyRequest}, or null if
+     *    there are no pending requests in those states
+     */
+    getOutgoingRoomKeyRequestByState(wantedStates) {
+        if (wantedStates.length === 0) {
+            return null;
+        }
+
+        // this is a bit tortuous because we need to make sure we do the lookup
+        // in a single transaction, to avoid having a race with the insertion
+        // code.
+
+        // index into the wantedStates array
+        let stateIndex = 0;
+        let result;
+
+        function onsuccess(ev) {
+            const cursor = ev.target.result;
+            if (cursor) {
+                // got a match
+                result = cursor.value;
+                return;
+            }
+
+            // try the next state in the list
+            stateIndex++;
+            if (stateIndex >= wantedStates.length) {
+                // no matches
+                return;
+            }
+
+            const wantedState = wantedStates[stateIndex];
+            const cursorReq = ev.target.source.openCursor(wantedState);
+            cursorReq.onsuccess = onsuccess;
+        }
+
+        return this.connect().then((db) => {
+            const txn = db.transaction("outgoingRoomKeyRequests", "readonly");
+            const store = txn.objectStore("outgoingRoomKeyRequests");
+
+            const wantedState = wantedStates[stateIndex];
+            const cursorReq = store.index("state").openCursor(wantedState);
+            cursorReq.onsuccess = onsuccess;
+
+            return promiseifyTxn(txn).then(() => result);
+        });
+    }
+
+    /**
+     * Look for an existing room key request by id and state, and update it if
+     * found
+     *
+     * @param {string} requestId      ID of request to update
+     * @param {number} expectedState  state we expect to find the request in
+     * @param {Object} updates        name/value map of updates to apply
+     *
+     * @returns {Promise} resolves to
+     *    {@link module:crypto/store/base~OutgoingRoomKeyRequest}
+     *    updated request, or null if no matching row was found
+     */
+    updateOutgoingRoomKeyRequest(requestId, expectedState, updates) {
+        let result = null;
+
+        function onsuccess(ev) {
+            const cursor = ev.target.result;
+            if (!cursor) {
+                return;
+            }
+            const data = cursor.value;
+            if (data.state != expectedState) {
+                console.warn(
+                    `Cannot update room key request from ${expectedState} ` +
+                    `as it was already updated to ${data.state}`,
+                );
+                return;
+            }
+            Object.assign(data, updates);
+            cursor.update(data);
+            result = data;
+        }
+
+        return this.connect().then((db) => {
+            const txn = db.transaction("outgoingRoomKeyRequests", "readwrite");
+            const cursorReq = txn.objectStore("outgoingRoomKeyRequests")
+                .openCursor(requestId);
+            cursorReq.onsuccess = onsuccess;
+            return promiseifyTxn(txn).then(() => result);
+        });
+    }
+
+    /**
+     * Look for an existing room key request by id and state, and delete it if
+     * found
+     *
+     * @param {string} requestId      ID of request to update
+     * @param {number} expectedState  state we expect to find the request in
+     *
+     * @returns {Promise} resolves once the operation is completed
+     */
+    deleteOutgoingRoomKeyRequest(requestId, expectedState) {
+        return this.connect().then((db) => {
+            const txn = db.transaction("outgoingRoomKeyRequests", "readwrite");
+            const cursorReq = txn.objectStore("outgoingRoomKeyRequests")
+                .openCursor(requestId);
+            cursorReq.onsuccess = (ev) => {
+                const cursor = ev.target.result;
+                if (!cursor) {
+                    return;
+                }
+                const data = cursor.value;
+                if (data.state != expectedState) {
+                    console.warn(
+                        `Cannot delete room key request in state ${data.state} `
+                        + `(expected ${expectedState})`,
+                    );
+                    return;
+                }
+                cursor.delete();
+            };
+            return promiseifyTxn(txn);
+        });
+    }
 }
 
 function createDatabase(db) {
@@ -123,4 +356,11 @@ function createDatabase(db) {
     );
 
     outgoingRoomKeyRequestsStore.createIndex("state", "state");
+}
+
+function promiseifyTxn(txn) {
+    return new q.Promise((resolve, reject) => {
+        txn.oncomplete = resolve;
+        txn.onerror = reject;
+    });
 }
